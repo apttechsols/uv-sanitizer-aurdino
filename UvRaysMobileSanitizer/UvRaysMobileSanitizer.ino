7c1e6ba68df0310a1587351d8221a2e107d4e4ed
Apt TechSols <apttechsols@gmail.com>
2020-12-08 13:50:17 +0530

    Update UvRaysMobileSanitizer.ino

--- UvRaysMobileSanitizer/UvRaysMobileSanitizer.ino
+++ UvRaysMobileSanitizer/UvRaysMobileSanitizer.ino
@@ -3,61 +3,87 @@
  Start Date : 19 JUN 2020
  Modified Date : 19 JUN 2020
  Dec : It is a Sanitizer machine which can sanitiz mobiles, cash, wallet, etc. with the help of uv rays
- Parner : GIT organization
 */
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x27,16,5);  // set the LCD address to 0x3f for upload ardeuno 0X3F
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x3f for upload ardeuno 0X3F
 // the setup function runs once when you press reset or power the board
 const int buttonPin = 2;
 int buttonState = 0;
 int DoorState = 0;
 int PreState = 0;
-int CurrentState = 0;
+int CurrentState = HIGH;
 int PushButtonCount = 0;
 int SanitiztionProcess = -1;
 int IsAlarmed = 0;
 int WaitTimeCount = 0;
 int IsUnsafe = 0;
+int IsCollacte = 0;
+const int pingPin = 10;
+const int echoPin = 11;
+const int UvOut = 4;
+int distance = 50000;
+int  IsStart = 0;
 void setup() {
   Serial.begin(9600);
   lcd.init();             // initialize the lcd 
   lcd.backlight();        // initialize the lcd 
-  lcd.begin(16, 5);
+  lcd.begin(20, 4);
   lcd.setCursor(0,0);
   lcd.print("GIT PROJECT");
   lcd.setCursor(0,1);
   lcd.print("cash sanitizer machine");
-
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  DDRD = 0x00;
+  PORTD = 0x01; 
+  // initialize digital pin UvOut as an output. YT 
+  pinMode(UvOut, OUTPUT);
   pinMode(7, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  pinMode(12, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
   pinMode(8, INPUT);
+  pinMode(pingPin, OUTPUT); // initialising pin 10 as output
+  pinMode(echoPin, INPUT); // initialising pin 11 as input
 
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-    
-    buttonState = digitalRead(buttonPin);
     DoorState = digitalRead(8);
+    long duration, inches, cm;
+  
+    digitalWrite(pingPin, LOW);
+    delayMicroseconds(2);
     
-    if (buttonState == HIGH){
-      CurrentState = HIGH;
-      //SanitizationTime(5);
-    }else{
-      CurrentState = LOW;
-    }
+    digitalWrite(pingPin, HIGH);
+    delayMicroseconds(10);
+    
+    digitalWrite(pingPin, LOW);
 
-    if(DoorState == LOW){
-      digitalWrite(LED_BUILTIN, LOW);
+  
+    duration = pulseIn(echoPin, HIGH); // using pulsin function to determine total time
+    inches = microsecondsToInches(duration); // calling method
+    cm = microsecondsToCentimeters(duration); // calling method
+    distance = cm;
+
+    if(distance < 20){ // change UltraSonic sensor lenth change
+      IsStart = 1;
+      CurrentState = HIGH;
+    }else{
+      if(IsStart != 1){
+        IsStart = 0;
+        CurrentState = LOW;
+      }
+    }
+    Serial.print(DoorState);
+
+    if(DoorState == HIGH){
+      digitalWrite(UvOut, LOW);
       digitalWrite(7, LOW);
-      digitalWrite(10, HIGH);
+      digitalWrite(12, HIGH);
+      IsStart = 0;
       lcd.setCursor(0,1);
-      if(SanitiztionProcess == 2){
+      if(SanitiztionProcess == 2 and IsUnsafe != 2){
         IsUnsafe = 1; 
       }
       if((SanitiztionProcess == 2 or SanitiztionProcess == -2) and IsUnsafe == 1){
@@ -65,71 +91,76 @@ void loop()
          SanitiztionProcess = -2;
          lcd.print("Door opend [Unsafe].................................");
       }else{
-        IsUnsafe = 0;
+        if(SanitiztionProcess == 1 or IsCollacte == 1 or IsUnsafe == 2){
+          IsCollacte = 1;
+        }else{
+          IsCollacte = 0;
+        }
+        IsUnsafe = 0;    
         SanitiztionProcess = -2;
-        lcd.print("Door opend.................................");
+        if(IsCollacte == 1){
+          lcd.print("Door opend[Collacte].................................");
+        }else{
+          lcd.print("Door opend.................................");
+        }
       }
       String  PushButtonCountMsg = "Cash sanitiz : ";
       PushButtonCountMsg.concat(PushButtonCount);
       lcd.setCursor(0,0);
       lcd.print(PushButtonCountMsg);
     }else if(CurrentState == HIGH and (SanitiztionProcess == 0 or SanitiztionProcess == 2)){
+      IsCollacte = 0;
       IsAlarmed = 0;
-      digitalWrite(10, LOW);
+      digitalWrite(12, LOW);
       digitalWrite(7, LOW);
       lcd.setCursor(0,1);
-      //lcd.print("Cash sanitiztion process is running...");
       lcd.print("running....................................");
-      digitalWrite(LED_BUILTIN, HIGH);
-      //SanitiztionProcess = 1;
-      //WaitSanitizationTime(5);
-      String  TmpWaitTime = "Wait time sec: ";
-      TmpWaitTime.concat((5 + 1) - WaitTimeCount);
+      digitalWrite(UvOut, HIGH);
+      String  TmpWaitTime = "Wait time :";
+      TmpWaitTime.concat((60*1) - WaitTimeCount); // Uv Led delay time
+      TmpWaitTime.concat("......");
       lcd.setCursor(0,0);
       lcd.print(TmpWaitTime);
-      delay(1000);
-      if(((5 + 1) - WaitTimeCount) <= 1){
+      delay(70);
+      if(((60*1) - WaitTimeCount) <= 1){  // Uv Led delay time
         SanitiztionProcess = 1;
+        PushButtonCount++;
       }else{
         SanitiztionProcess = 2;
       }
       WaitTimeCount++;
     }else if(CurrentState == HIGH and SanitiztionProcess == 1){
+      digitalWrite(UvOut, LOW);
+      IsCollacte = 0;
+      IsUnsafe = 2;
       WaitTimeCount = 0;
-      digitalWrite(10, LOW);
-      digitalWrite(LED_BUILTIN, LOW);
+      digitalWrite(12, LOW);
       lcd.setCursor(0,1);
-      //lcd.print("Cash sanitizeration process completed");
       lcd.print("completed..................................");
-      digitalWrite(LED_BUILTIN, LOW);
       if(IsAlarmed == 0){
-        PushButtonCount++;
         digitalWrite(7, HIGH);
-        WaitAlarmTime(2);
+        WaitAlarmTime(20);   // Alarm delay time
         digitalWrite(7, LOW);
         IsAlarmed = 1;
       }
     }else{
-      WaitTimeCount = 0;
-      digitalWrite(10, LOW);
-      digitalWrite(LED_BUILTIN, LOW);
+      digitalWrite(UvOut, LOW);
+      digitalWrite(12, LOW);
       digitalWrite(7, LOW);
+      WaitTimeCount = 0;
+      IsCollacte = 0;
       lcd.setCursor(0,1);
-      //lcd.print("Welcome in cash sanitization machine...");
-      if(CurrentState == HIGH and (SanitiztionProcess == -1 or SanitiztionProcess == -2)){
-        digitalWrite(LED_BUILTIN, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(7, LOW);
-        lcd.print("Do ready...................................");
-      }else if(CurrentState == LOW and (SanitiztionProcess == -1 or SanitiztionProcess == -2)){
+      if(SanitiztionProcess == -1 or SanitiztionProcess == -2){
          SanitiztionProcess = 0;
       }else if(SanitiztionProcess == 0 or SanitiztionProcess == 1){
         SanitiztionProcess = 0;
-        IsUnsafe = 0; 
+        if(IsUnsafe != 2){
+          IsUnsafe = 0;
+        } 
         lcd.print("Ready...................................");
       }else{
-        digitalWrite(LED_BUILTIN, LOW);
-        digitalWrite(10, HIGH);
+        digitalWrite(UvOut, LOW);
+        digitalWrite(12, HIGH);
         lcd.print("Cancelled...................................");
       }
       IsAlarmed = 1;
@@ -143,23 +174,20 @@ void loop()
       lcd.print(PushButtonCountMsg);
     }
 }
-
-void WaitSanitizationTime(int timesec){
+void WaitAlarmTime(int timesec){
   for(int i = 0; i <= timesec; i++){
-    String  WaitTime = "Wait time sec: ";
-    WaitTime.concat((timesec + 1) - i);
+    String  WaitTime = "Alarming for: ";
+    WaitTime.concat((timesec) - i);
+     WaitTime.concat(".........");
     lcd.setCursor(0,0);
     lcd.print(WaitTime);
-    delay(1000);
+    delay(125);
   }
 }
 
-void WaitAlarmTime(int timesec){
-  for(int i = 0; i <= timesec; i++){
-    String  WaitTime = "Alarming for : ";
-    WaitTime.concat((timesec + 1) - i);
-    lcd.setCursor(0,0);
-    lcd.print(WaitTime);
-    delay(1000);
-  }
+long microsecondsToInches(long microseconds){
+  return microseconds / 74 / 2;
+}
+long microsecondsToCentimeters(long microseconds){
+  return microseconds / 29 / 2;
 }
